27689812228356cf119decbc694ffe707d20c75d
Janitha Missaka <janitha.missaka@gmail.com>
2021-04-08 14:21:22 +0530

    Add model and CSV library

--- src/main/java/com/covidtracker/demo/Service/CoronaDataService.java
+++ src/main/java/com/covidtracker/demo/Service/CoronaDataService.java
@@ -1,19 +1,26 @@
 package com.covidtracker.demo.Service;
+import com.covidtracker.demo.Service.Models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaDataService {
 
     private static String DataURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2021.csv";
-
+    private List<LocationStats> allstats = new ArrayList<>();
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -21,8 +28,18 @@ public class CoronaDataService {
                 .build();
 
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        //System.out.println(httpResponse.body());
 
+        StringReader stringReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(stringReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province_State");
+            System.out.println(state);
+//            String customerNo = record.get("CustomerNo");
+//            String name = record.get("Name");
+        }
     }
+
+
 
 }
